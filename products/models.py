cf19a813bf8769077f825117aff8dfec16c75331
AhsanZak <ahsanzakpri@gmail.com>
2023-10-10 00:24:11 +0530

    Added products app, defined the models

---
+++ products/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    brand = models.ForeignKey(Brand, on_delete=models.DO_NOTHING)
+    product_type = models.ForeignKey(ProductType, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
